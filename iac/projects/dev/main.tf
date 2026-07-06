58bac795eff655dbfdaeac782fe6892d1c07f354
ikenley <ikenley6@gmail.com>
2026-07-06 05:56:28 -0400

    init

---
+++ iac/projects/dev/main.tf
@@ -0,0 +1,36 @@
+# ------------------------------------------------------------------------------
+# email-ai-agent
+# Inbound email AI chat agent
+# ------------------------------------------------------------------------------
+
+terraform {
+  required_version = ">= 0.14"
+
+  backend "s3" {
+    profile = "terraform-dev"
+    region  = "us-east-1"
+    bucket  = "924586450630-terraform-state"
+    key     = "email-ai-agent/dev/terraform.tfstate.json"
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "terraform-dev"
+}
+
+# ------------------------------------------------------------------------------
+# Resources
+# ------------------------------------------------------------------------------
+
+module "main" {
+  source = "../../modules/main"
+
+  namespace = "ik"
+  env       = "dev"
+  is_prod   = false
+
+  inbound_email_address = var.inbound_email_address
+
+  allowed_email_addresses_dynamo_table_name = var.allowed_email_addresses_dynamo_table_name
+}
---
+++ iac/projects/dev/terraform.tfvars
@@ -0,0 +1,2 @@
+inbound_email_address                     = "ikenley6@gmail.com"
+allowed_email_addresses_dynamo_table_name = "ik-dev-main-core-cognito-pre-signup-trigger"
